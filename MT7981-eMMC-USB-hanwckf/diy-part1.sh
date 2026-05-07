88a6d6d68fc8d8cb28efd08bbec4e322f72ef80b
Kylin <37763338+Kylinink@users.noreply.github.com>
2026-05-07 23:44:18 +0800

    Add files via upload

--- MT7981-eMMC-USB-hanwckf/diy-part1.sh
+++ MT7981-eMMC-USB-hanwckf/diy-part1.sh
@@ -10,8 +10,21 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 移除要替换的包
+rm -rf feeds/luci/applications/luci-app-lucky
+rm -rf feeds/luci/applications/luci-app-openclash
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -19,3 +32,5 @@
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist2
 git clone https://github.com/EasyTier/luci-app-easytier package/easytier
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
